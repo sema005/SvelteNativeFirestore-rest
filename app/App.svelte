bd3697d071b541e2c4bf31c3a773b8d26cbb8397
Sebastian <sebastianmar00@gmail.com>
2020-03-24 12:13:08 +0100

    changes

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,53 @@
-<page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
 <script>
-    let message = "Blank Svelte Native App"
+    import FirestoreParser from "firestore-parser"
+
+    let items = []
+
+    const baseUrl = "https://firestore.googleapis.com/v1/"
+    const productsUrl = baseUrl + "project/svelte-native/databases/(default)/documents/products"
+
+    const getProducts = () => {
+        fetch(productsUrl)
+        .then( response => response.json() )
+            .then( json => FirestoreParser(json) )
+                .then( parsed => {
+                    items = parsed.documents 
+                    console.log(items)
+                    })
+        .catch( error => console.log(error) )
+    }
+
+getProducts()
+
 </script>
 
+<page>
+    <actionBar title="Svelte firestore rest" />
+    <scrollView class="main ">
+        <stackLayout>
+            {#each items as item}
+                <flexboxLayout class="product" >
+                    <image src={item.fields.image} stretch="aspectFit" />
+                    <label class="h2 " text={item.fields.title} />
+                </flexboxLayout>
+            {:else}
+                <activityIndicator busy={true} />
+            {/each}
+        </stackLayout>
+    </scrollView>
+</page>
+
 <style>
-    .info .fas {
-        color: #3A53FF;
+    .main {
+        background-color: #444;
     }
-    .info {
-        font-size: 20;
+    .product {
+        margin: 8 8 0 8;
+        background-color: lightgray;
+        border-radius: 8;
+    }
+    .product > image {
+        height: 80;
+        margin-right: 16;
     }
 </style>
