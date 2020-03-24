6acabc04de1ea44f146291e3de9a557cbbbe4740
Sebastian <sebastianmar00@gmail.com>
2020-03-24 12:25:38 +0100

    working

--- app/App.svelte
+++ app/App.svelte
@@ -1,34 +1,31 @@
 <script>
-    import FirestoreParser from "firestore-parser"
-
-    let items = []
-
-    const baseUrl = "https://firestore.googleapis.com/v1/"
-    const productsUrl = baseUrl + "project/svelte-native/databases/(default)/documents/products"
-
-    const getProducts = () => {
-        fetch(productsUrl)
-        .then( response => response.json() )
-            .then( json => FirestoreParser(json) )
-                .then( parsed => {
-                    items = parsed.documents 
-                    console.log(items)
-                    })
-        .catch( error => console.log(error) )
-    }
-
+ import FirestoreParser from 'firestore-parser'
+ let items =[]
+ const baseUrl = 'https://firestore.googleapis.com/v1/'
+ const productsUrl = 
+ baseUrl + 'projects/svelte-native/databases/(default)/documents/products'
+const getProducts = () => {
+    fetch(productsUrl)
+    .then( response => response.json() )
+        .then( json => FirestoreParser(json) )
+            .then( parsed => {
+                items = parsed.documents
+            })
+    .catch( error => console.log(error) )
+}
 getProducts()
-
 </script>
 
 <page>
-    <actionBar title="Svelte firestore rest" />
-    <scrollView class="main ">
+    <actionBar title="Svelte Firestore REST" />
+    <scrollView class='main'>
         <stackLayout>
             {#each items as item}
-                <flexboxLayout class="product" >
-                    <image src={item.fields.image} stretch="aspectFit" />
-                    <label class="h2 " text={item.fields.title} />
+                <flexboxLayout class='product'>
+                    <image src={item.fields.image} stretch='aspectFit' />
+                    <stackLayout>                
+                        <label class='h1' text={item.fields.title} />
+                    </stackLayout>
                 </flexboxLayout>
             {:else}
                 <activityIndicator busy={true} />
@@ -37,17 +34,19 @@ getProducts()
     </scrollView>
 </page>
 
+
 <style>
-    .main {
-        background-color: #444;
+    .main{
+        background-color:#aaa;
     }
-    .product {
+    .product{
         margin: 8 8 0 8;
-        background-color: lightgray;
-        border-radius: 8;
+        background-color:lightgray;
+        border-radius:8;
+        padding:16;
     }
-    .product > image {
-        height: 80;
-        margin-right: 16;
+    .product > image{
+        height:80;
+        margin-right:16;
     }
 </style>
